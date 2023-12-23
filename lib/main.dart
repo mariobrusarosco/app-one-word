d44ec00254cdfb0b447ab8aba716ea3d0175cf6f
Mario Brusarosco <mariobrusarosco@hotmail.com>
2023-12-23 10:08:59 -0300

    [LEARNING/FLUTTER] - using material design Card

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.pink,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(
                 radius: 50,
@@ -37,21 +38,23 @@ class MyApp extends StatelessWidget {
                   letterSpacing: 0.1,
                 ),
               ),
-              Container(
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.access_time_rounded,
-                      color: Colors.white,
-                    ),
-                    Text(
-                      "Coming Soon!",
-                      style: TextStyle(
-                        color: Colors.white,
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 40, horizontal: 20),
+                child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.access_time_rounded,
+                        color: Colors.pink,
                       ),
-                    ),
-                  ],
-                ),
+                      title: Text(
+                        "Coming Soon!",
+                        style: TextStyle(
+                          color: Colors.pink,
+                        ),
+                      ),
+                    )),
               ),
             ],
           ),
@@ -60,27 +63,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-// Container(
-// // margin: EdgeInsets.only(top: 50.0),
-// padding: EdgeInsets.symmetric(horizontal: 16, vertical: 24),
-// color: Colors.orange,
-// child: Container(
-// color: Colors.orange,
-// child: Text('Header'),
-// ),
-// ),
-// Container(
-// padding: EdgeInsets.symmetric(horizontal: 16, vertical: 24),
-// color: Colors.red,
-// child: Column(
-// crossAxisAlignment: CrossAxisAlignment.stretch,
-// children: [
-// Container(
-// child: Text('body'),
-// ),
-// ],
-// ),
-// ),
-// ]),
