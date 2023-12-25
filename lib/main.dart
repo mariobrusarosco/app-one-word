d0fa741786f4ded79446c01ef795998551ac3d25
Mario Brusarosco <mariobrusarosco@hotmail.com>
2023-12-25 17:57:24 -0300

    feat(App): Dice App

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,71 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(DiceUdemy());
 }
 
-class MyApp extends StatelessWidget {
+class DiceUdemy extends StatefulWidget {
+  @override
+  _DiceUdemyState createState() => _DiceUdemyState();
+}
+
+class _DiceUdemyState extends State<DiceUdemy> {
+  var leftDiceValue = 1;
+  var rightDiceValue = 1;
+
+  @override
+  Widget build(BuildContext context) {
+      int randomDiceValue () {
+        return Random().nextInt(6) + 1;
+      }
+
+      return MaterialApp(
+          home: Scaffold(
+            backgroundColor: Colors.deepOrangeAccent,
+            body: SafeArea(
+              child: Center(
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextButton(
+                        onPressed: () {
+                          setState(() {
+                            leftDiceValue = randomDiceValue();
+                          });
+                        },
+                        child: Image.asset(
+                          "assets/images/dice$leftDiceValue.png",
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: TextButton(
+                        onPressed: () {
+                          setState(() {
+                            rightDiceValue = randomDiceValue();
+                          });
+                        },
+                        child: Image.asset(
+                          "assets/images/dice$rightDiceValue.png",
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            appBar: AppBar(
+              title: Text("Dices"),
+              backgroundColor: Colors.orangeAccent,
+            ),
+          ));
+    }
+  }
+}
+
+class MyApp {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,41 +75,6 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Dices"),
           backgroundColor: Colors.orangeAccent,
-        ),
-      ),
-    );
-  }
-}
-
-class DiceUdemy extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var leftDiceValue = 1;
-    var rightDiceValue = 1;
-
-    return SafeArea(
-      child: Center(
-        child: Row(
-          children: [
-            Expanded(
-              child: TextButton(
-                onPressed: () => print("Left dice pressed"),
-                child: Image.asset(
-                  "assets/images/dice$leftDiceValue.png",
-                ),
-              ),
-            ),
-            Expanded(
-              child: TextButton(
-                onPressed: () {
-                  print("Left dice pressed");
-                },
-                child: Image.asset(
-                  "assets/images/dice$rightDiceValue.png",
-                ),
-              ),
-            )
-          ],
         ),
       ),
     );
