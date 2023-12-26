295369226a3e5188f32e71f88c534af1c87bdf72
Mario Brusarosco <mariobrusarosco@hotmail.com>
2023-12-26 08:59:19 -0300

    [LEARNING/FLUTTER] - Xylophone App

--- lib/main.dart
+++ lib/main.dart
@@ -3,30 +3,51 @@ import 'dart:math';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(Xylophone());
-}
+void main() => runApp(Xylophone());
 
 // XYLOPHONE APP
 class Xylophone extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // create a variable that holds a audio path string plus a color from Colors class
+    var audioPath = {
+      "audios/note.wav": Colors.red,
+      "audios/note2.wav": Colors.orange,
+      "audios/note3.wav": Colors.yellow,
+      "audios/note4.wav": Colors.green,
+      "audios/note5.wav": Colors.teal,
+      "audios/note6.wav": Colors.blue,
+      "audios/note6.wav": Colors.purple,
+    };
+
+    Expanded buildInstrumentTile(
+        {required String audioPath, required Color color}) {
+      return Expanded(
+        child: MaterialButton(
+          color: color,
+          onPressed: () async {
+            print(audioPath);
+            final player = AudioPlayer();
+            await player.play(AssetSource(audioPath));
+          },
+        ),
+      );
+    }
+
     return MaterialApp(
       title: "Xylophone",
       home: Scaffold(
-        backgroundColor: Colors.indigoAccent,
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(
-            child: FloatingActionButton(
-              onPressed: () async {
-                final player = AudioPlayer();
-                await player.play(AssetSource('audios/note.wav'));
-              },
-              // await player.play(AssetSource('assets/audios/note.wav')),
-              child: Text(
-                "Play",
-              ),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              for (var i = 0; i < audioPath.length; i++)
+                buildInstrumentTile(
+                  audioPath: audioPath.keys.elementAt(i),
+                  color: audioPath.values.elementAt(i),
+                ),
+            ],
           ),
         ),
       ),
